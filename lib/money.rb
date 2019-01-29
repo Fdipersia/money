2f654f2d9c5ade1335d76c5d683f8a83c6b3855c
Francisco Di Persia <fdipersia@gmail.com>
2019-01-28 23:44:03 -0300

    Create directory, gemfile and gemspec

---
+++ lib/money.rb
@@ -0,0 +1,90 @@
+class Money
+  attr_accessor :amount, :currency
+
+  #Set defaults
+  @@base_currency = 'EUR'
+  @@conversion_rates = {
+    'EUR' => 1,
+    'USD' => 1.11,
+    'Bitcoin' => 0.0047
+  }
+
+  #Constructor
+  def initialize(amount, currency)
+    @amount = amount.to_f
+    @currency = currency
+  end
+
+  #Convert method
+  def convert_to(to_currency)
+    if @@conversion_rates[to_currency].nil?
+      puts 'Rate not found'
+    else
+      from_currency = self.currency
+      rate = Money.getRate(from_currency, to_currency)
+    
+      converted_amount = self.amount * rate
+
+      Money.new(converted_amount, to_currency)
+    end
+  end
+
+  #Arithmetics
+  def other_amount(other)
+    rate = Money.getRate(other.currency, self.currency)
+    other.amount * rate
+  end
+
+  def +(other)
+    Money.new(self.amount + other_amount(other), self.currency)
+  end
+
+  def -(other)
+    Money.new(self.amount - other_amount(other), self.currency)
+  end
+
+  def *(number)
+    Money.new(self.amount * number.to_f, self.currency)
+  end
+
+  def /(number)
+    Money.new(self.amount / number.to_f, self.currency)
+  end
+
+  #Comparisons
+  def ==(other)
+    temp = other.convert_to(self.currency)
+    return self.amount.round(2) == temp.amount.round(2)
+  end
+
+  def >(other)
+    temp = other.convert_to(self.currency)
+    return self.amount.round(2) > temp.amount.round(2)
+  end
+
+  def <(other)
+    temp = other.convert_to(self.currency)
+    return self.amount.round(2) < temp.amount.round(2)
+  end
+
+  def inspect
+    return sprintf('%.2f %s', amount, currency)
+  end
+
+  def to_s
+    return sprintf('%.2f %s', amount, currency)
+  end
+
+  def self.getRate(from_currency, to_currency)
+    return 1 if to_currency == from_currency
+
+    if !@@conversion_rates[to_currency].nil? && from_currency == @@base_currency
+      return @@conversion_rates[to_currency]
+    end
+
+    if !@@conversion_rates[from_currency].nil? && to_currency == @@base_currency
+      return 1.0 / @@conversion_rates[from_currency]
+    end
+  end
+
+end
---
+++ money.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'money_handler'
+  s.version     = '0.0.0'
+  s.date        = '2019-01-29'
+  s.summary     = "This gem handles different currencies"
+  s.authors     = ["Francisco Di Persia"]
+  s.email       = 'fdipersia@gmail.com'
+  s.files       = ["lib/money.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/money_handler'
+  s.license     = 'MIT'
+end
